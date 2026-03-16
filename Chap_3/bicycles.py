1d223c76e1ca9de2d8aac7427b5173d2f51f6697
Naresh-Barua <nareshbarua72@gmail.com>
2026-03-17 00:20:21 +0600

    new

--- Chap_3/bicycles.py
+++ Chap_3/bicycles.py
@@ -1,6 +1,9 @@
 bicycles = ['trek', 'cannondale','redline','specialized']
+msg = f"My first bike was a {bicycles[0].title()}"
 # print(bicycles)
-
-bicycles = ['trek', 'cannondale','redline','specialized']
 # print(bicycles[3])
+# print(bicycles[-1])
+# print(msg)
+
+
 
